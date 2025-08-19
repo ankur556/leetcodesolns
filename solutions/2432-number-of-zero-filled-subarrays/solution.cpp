a6644291ea05ac95535c83eda8b2bf645fc5146b
ankur556 <ankurclasher2@gmail.com>
2025-08-19 04:49:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 111.4 MB (41.71%)

---
+++ solutions/2432-number-of-zero-filled-subarrays/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+       long long count=0;
+       long long streak=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0){
+                streak++;
+                count+=streak;
+            }
+            else{streak=0;}
+            }
+            return count ;
+        }
+       
+    };
