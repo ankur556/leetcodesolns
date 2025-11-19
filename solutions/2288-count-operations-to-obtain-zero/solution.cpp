5aa28600b65b42826b0b650a79f1c8bfd6010b4b
ankur556 <ankurclasher2@gmail.com>
2025-11-19 21:06:23 +0000

    Sync LeetCode submission Runtime - 3 ms (14.40%), Memory - 8 MB (37.05%)

---
+++ solutions/2288-count-operations-to-obtain-zero/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int i=0;
+        if(num1==0||num2==0){return 0;}
+     while(num1!=num2){
+        if(num1>num2){i++;num1=num1-num2;}
+        else{i++;num2=num2-num1;}
+     }
+     return i+1;
+    }
+};
