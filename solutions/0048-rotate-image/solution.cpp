07063895267087735ace6cc6dc9dd30934dc9599
ankur556 <ankurclasher2@gmail.com>
2025-12-16 17:20:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.1 MB (68.37%)

---
+++ solutions/0048-rotate-image/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        vector<int> a;
+        for(int i=0;i<matrix.size();i++){
+            for(int j=i;j<matrix[0].size();j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<matrix.size()/2;j++){
+            swap(matrix[i][j],matrix[i][matrix.size()-j-1]);
+            }
+        }
+    }
+};
