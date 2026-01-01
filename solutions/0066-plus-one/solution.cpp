1994c20574686254b5d2f5f443acca346c7bd3c0
ankur556 <ankurclasher2@gmail.com>
2026-01-01 10:34:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.6 MB (27.12%)

--- solutions/0066-plus-one/solution.cpp
+++ solutions/0066-plus-one/solution.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         int no_of_9s=0;
-        for(int i=0;i<digits.size();i++){
-            if(digits[i]==9){no_of_9s++;}
-        }
+        vector<int> sol;
+        for(int i=0;i<digits.size();i++){if(digits[i]==9){no_of_9s++;}}
         if(no_of_9s==digits.size()){
-            vector<int> ans;
-            ans.push_back(1);
+            sol.push_back(1);
             for(int i=0;i<digits.size();i++){
-                ans.push_back(0);
+                sol.push_back(0);
             }
-            return ans;
+            return sol;
+        
         }
         else{
-            for(int i=0;i<digits.size();i++){
-                if(digits[digits.size()-i-1]==9){digits[digits.size()-i-1]=0;}
-                else{digits[digits.size()-i-1]=digits[digits.size()-i-1]+1;break;}
-            }
+            int a=digits.size()-1;
+            while(digits[a]==9){digits[a]=0;a--;}
+            digits[a]++;
             return digits;
         }
     }
