6c69ce369787326a3eb1fce5004d156e0d32bc07
ankur556 <ankurclasher2@gmail.com>
2026-02-10 06:46:54 +0000

    Sync LeetCode submission Runtime - 1991 ms (32.37%), Memory - 530.5 MB (28.74%)

---
+++ solutions/4045-longest-balanced-subarray-i/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int maxLen = 0;
+
+        for (int i = 0; i < n; i++) {
+            unordered_set<int> distinctEvens;
+            unordered_set<int> distinctOdds;
+
+            for (int j = i; j < n; j++) {
+                // Classify the current number
+                if (nums[j] % 2 == 0) {
+                    distinctEvens.insert(nums[j]);
+                } else {
+                    distinctOdds.insert(nums[j]);
+                }
+
+                // Check if the current subarray [i...j] is balanced
+                if (distinctEvens.size() == distinctOdds.size()) {
+                    maxLen = max(maxLen, j - i + 1);
+                }
+            }
+        }
+
+        return maxLen;
+    }
+};
