4f5357d4b900c2e00766329f00a9c9f63f429897
ankur556 <ankurclasher2@gmail.com>
2026-02-19 19:51:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (32.72%)

---
+++ solutions/0392-is-subsequence/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+       int i=0;//int j=0;
+        for(int j=0;j<t.size();j++){
+            if(t[j]==s[i]){i++;}
+        }
+        return i==s.size();
+   }
+};
