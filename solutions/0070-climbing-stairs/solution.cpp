7bf1f42fe61b8fc6e1fd260d9a65333cc09e433d
ankur556 <ankurclasher2@gmail.com>
2026-04-04 11:20:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (18.68%)

--- solutions/0070-climbing-stairs/solution.cpp
+++ solutions/0070-climbing-stairs/solution.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        int prev = 1, curr = 1;
-        for (int i = 2; i <= n; i++) {
-            int temp = curr;
-            curr = prev + curr;
-            prev = temp;
-        }
-        return curr;
+        if(n==1){return 1;}
+        else if(n==2){return 2;}
+        vector<int>dp(n+1);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<n+1;i++){dp[i]=dp[i-1]+dp[i-2];}
+        return dp[n];
     }
 };
