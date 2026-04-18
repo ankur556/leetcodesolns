da8cb961d7659acfbc6fa95382a4f82f4487f8ff
ankur556 <ankurclasher2@gmail.com>
2026-04-18 05:04:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (58.87%)

---
+++ solutions/4168-mirror-distance-of-an-integer/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        int k=n;
+        int rev=0;
+        while(k%10==0&&k!=0){k=k/10;}
+        while(k>0){rev=rev*10+k%10;k/=10;}
+        return abs(n-rev);
+    }
+};
