e780295e05bd6c73d99edc717e7f63127cdad854
ankur556 <ankurclasher2@gmail.com>
2026-05-13 22:48:17 +0000

    Sync LeetCode submission Runtime - 7 ms (40.31%), Memory - 16.7 MB (5.19%)

--- solutions/0001-two-sum/solution.cpp
+++ solutions/0001-two-sum/solution.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int> a;
-        for(int i=0;i<nums.size();i++){a[nums[i]]=i;}
         vector<int> sol;
-        for(int i=1;i<nums.size();i++){if(nums[i]+nums[0]==target){sol.push_back(i);sol.push_back(0);return sol;}
-        else if(a[target-nums[i]]!=i&&a[target-nums[i]]!=0){sol.push_back(i);sol.push_back(a[target-nums[i]]);return sol;}}
-        sol.push_back(0);
-        sol.push_back(nums.size()-1);
+        for(int i=0;i<nums.size();i++){a[nums[i]]=i+1;}
+        for(int i=0;i<nums.size();i++){if(a[target-nums[i]]!=0&&(a[target-nums[i]]!=i+1)){sol.push_back(i);sol.push_back(a[target-nums[i]]-1);break;}}
         return sol;
     }
 };
