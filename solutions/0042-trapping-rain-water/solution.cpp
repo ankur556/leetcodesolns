9fdcba9475dd14d14ab8629cab74c77027e2fa83
ankur556 <ankurclasher2@gmail.com>
2026-05-14 16:03:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26 MB (57.26%)

--- solutions/0042-trapping-rain-water/solution.cpp
+++ solutions/0042-trapping-rain-water/solution.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int lmax_index=0;
-        int rmax_index=height.size()-1;
-        int l_max=height[0];
-        int r_max=height[height.size()-1];
-        int ans=0;
-        while(lmax_index!=rmax_index){
-            if(r_max>l_max){
-                lmax_index++;
-                if(height[lmax_index]>l_max){
-                    l_max=height[lmax_index];
-                }
-                else{ans+=(l_max-height[lmax_index]);}
-            }
-            else{
-                rmax_index--;
-                if(height[rmax_index]>r_max){
-                    r_max=height[rmax_index];
-                }
-                else{ans+=(r_max-height[rmax_index]);}
-            }
+        int sol=0;
+        int l=0;
+        int r=height.size()-1;
+        int max_l=height[0];
+        int max_r=height[height.size()-1];
+        int h=min(max_l,max_r);
+        
+        while(l<r){
+            max_l=max(height[l],max_l);
+            max_r=max(height[r],max_r);
+            h=min(max_l,max_r);
+            if(height[r]>height[l]){sol+=(h-height[l]);l++;}
+            else{sol+=(h-height[r]);r--;}
         }
-        return ans;
+        return sol;
     }
 };
