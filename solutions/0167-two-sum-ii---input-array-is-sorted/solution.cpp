9162e7227d01dc685ff775bbb4fb16b54db8618b
ankur556 <ankurclasher2@gmail.com>
2026-05-17 09:17:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.6 MB (33.51%)

--- solutions/0167-two-sum-ii---input-array-is-sorted/solution.cpp
+++ solutions/0167-two-sum-ii---input-array-is-sorted/solution.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int low=0;
-        int high=numbers.size()-1;
-        while(high>low){
-            if(numbers[high]+numbers[low]>target){
-                high--;
-            }
-            else if(numbers[high]+numbers[low]<target){low++;}
-            else if(numbers[high]+numbers[low]==target){vector<int> a;a.push_back(low+1);a.push_back(high+1);return a;}
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int r=nums.size()-1;
+        int l=0;
+        vector<int> sol;
+        while(l<r){
+            if(nums[l]+nums[r]==target){sol.push_back(l+1);sol.push_back(r+1);break;}
+            else if(nums[l]+nums[r]<target){l++;}
+            else{r--;}
         }
-        vector<int> t;
-        t.push_back(1);
-        t.push_back(2);
-        return t;
-        
+        return sol;
     }
 };
