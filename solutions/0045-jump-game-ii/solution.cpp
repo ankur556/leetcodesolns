aab8592ccadb22e3acffdd2c2b6c0f07d76798b2
ankur556 <ankurclasher2@gmail.com>
2026-05-17 09:08:23 +0000

    Sync LeetCode submission Runtime - 63 ms (20.92%), Memory - 21.6 MB (24.00%)

--- solutions/0045-jump-game-ii/solution.cpp
+++ solutions/0045-jump-game-ii/solution.cpp
@@ -1,28 +1,17 @@
+#include<bits/stdc++.h>
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        // Edge case: If array length is 1, we are already at the end.
-        if (nums.size() <= 1) return 0;
-
-        int jumps = 0;
-        int current_end = 0;
-        int farthest = 0;
-
-        // We iterate to nums.size() - 1 because we don't need to jump FROM the last element.
-        for (int i = 0; i < nums.size() - 1; i++) {
-            // Update the furthest point reachable from the current index i
-            farthest = max(farthest, i + nums[i]);
-
-            // If we have reached the end of the current jump range
-            if (i == current_end) {
-                jumps++;             // We must take a jump
-                current_end = farthest; // Update the range for the next jump
-
-                // Optimization: If the new range already covers the last index, break early
-                if (current_end >= nums.size() - 1) break;
+        vector<int>dp(nums.size(),-1);
+        dp[0]=0;
+        for(int i=0;i<nums.size();i++){
+            if(dp[i]>=0){
+                for(int j=i;j<=nums.size()-1&&j<=i+nums[i];j++){
+                    if(dp[j]==-1){dp[j]=dp[i]+1;}
+                    if(dp[i]+1<dp[j]){dp[j]=dp[i]+1;}
+                }
             }
         }
-
-        return jumps;
+        return dp[dp.size()-1];
     }
 };
