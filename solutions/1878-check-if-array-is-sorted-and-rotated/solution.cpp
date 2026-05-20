eb46a7e085ed68e7bb0f1861ba7822ea7b0c97ce
ankur556 <ankurclasher2@gmail.com>
2026-05-20 15:26:13 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.5 MB (8.89%)

--- solutions/1878-check-if-array-is-sorted-and-rotated/solution.cpp
+++ solutions/1878-check-if-array-is-sorted-and-rotated/solution.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int drops=0;
-        for(int i=0;i<nums.size()-1;i++){
-            if(nums[i+1]<nums[i]){drops++;}
+        vector<int> b;
+        if(nums.size()==1){return true;}
+        for(int i:nums){b.push_back(i);}
+        for(int i:nums){b.push_back(i);}
+        int cur=1;
+        for(int i=1;i<b.size();i++){
+            
+            if(b[i]>=b[i-1]){cur++;}
+            else{cur=1;}
+            if(cur==nums.size()){return true;}
         }
-        if(nums[nums.size()-1]>nums[0]){drops++;}
-        return drops<=1;
+        return false;    
     }
 };
