b00c0fef7a7196da486229fd2866e903e2aa2bf5
ankur556 <ankurclasher2@gmail.com>
2026-05-20 20:58:27 +0000

    Sync LeetCode submission Runtime - 4 ms (18.48%), Memory - 28.2 MB (65.78%)

--- solutions/0169-majority-element/solution.cpp
+++ solutions/0169-majority-element/solution.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int mid=(nums.size())/2;
-        return nums[mid];
+        unordered_map<int,int> a;
+        for(int i=0;i<nums.size();i++){
+            a[nums[i]]++;
+            if(a[nums[i]]>nums.size()/2){return nums[i];}
+        }
+        return nums[nums.size()-1];
     }
 };
