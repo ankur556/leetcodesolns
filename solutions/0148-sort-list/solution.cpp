fd9a48ebf9e5dddda2416dd91ac4b3926b5647a8
ankur556 <ankurclasher2@gmail.com>
2026-05-21 13:02:41 +0000

    Sync LeetCode submission Runtime - 12 ms (74.98%), Memory - 58.1 MB (67.45%)

--- solutions/0148-sort-list/solution.cpp
+++ solutions/0148-sort-list/solution.cpp
@@ -1,50 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* sortList(ListNode* head) {
-        if (!head || !head->next) return head; // Base case
-
-        // 1. Split list into two halves
-        ListNode* mid = getMiddle(head);
-        ListNode* right = mid->next;
-        mid->next = nullptr; // Cut the list
-
-        // 2. Sort each half
-        ListNode* leftSorted = sortList(head);
-        ListNode* rightSorted = sortList(right);
-
-        // 3. Merge sorted halves
-        return merge(leftSorted, rightSorted);
-    }
-
-private:
-    // Find middle using slow/fast pointers
-    ListNode* getMiddle(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast && fast->next) {
-            slow = slow->next;
-            fast = fast->next->next;
+        vector<int> helper;
+        ListNode* a=head;
+        while(head!=nullptr){
+            helper.push_back(head->val);
+            head=head->next;
         }
-        return slow;
-    }
-
-    // Merge two sorted linked lists
-    ListNode* merge(ListNode* l1, ListNode* l2) {
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
-
-        while (l1 && l2) {
-            if (l1->val < l2->val) {
-                tail->next = l1;
-                l1 = l1->next;
-            } else {
-                tail->next = l2;
-                l2 = l2->next;
-            }
-            tail = tail->next;
+        head=a;
+        sort(helper.begin(),helper.end());
+        for(int i=0;i<helper.size();i++){
+            head->val=helper[i];
+            head=head->next;
         }
-        tail->next = l1 ? l1 : l2;
-        return dummy.next;
+        head=a;
+        return head;
     }
 };
-
