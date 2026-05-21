681b945354902e91245addf046889248bc9b4ad1
ankur556 <ankurclasher2@gmail.com>
2026-05-21 12:49:11 +0000

    Sync LeetCode submission Runtime - 2 ms (4.22%), Memory - 8.5 MB (23.79%)

--- solutions/1737-maximum-nesting-depth-of-the-parentheses/solution.cpp
+++ solutions/1737-maximum-nesting-depth-of-the-parentheses/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        int count=0;
-        int max=0;
+        int cur=0;
+        int sol=0;
         for(char c: s){
-            if(c=='('){count++;}
-            else if(c==')'){count--;}
-            if(count>max){max=count;}
+            if(c=='('){cur++;}
+            else if(c==')'){cur--;}
+            if(cur>sol){sol=cur;}
         }
-        return max;
+        return sol;
     }
 };
