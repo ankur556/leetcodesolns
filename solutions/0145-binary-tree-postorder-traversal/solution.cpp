cbb86d441f9772b677318b2f891d8de7aac03f8c
ankur556 <ankurclasher2@gmail.com>
2026-05-22 19:59:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.1 MB (44.09%)

--- solutions/0145-binary-tree-postorder-traversal/solution.cpp
+++ solutions/0145-binary-tree-postorder-traversal/solution.cpp
@@ -13,15 +13,16 @@ class Solution {
 public:
     void helper(vector<int> & sol,TreeNode* a){
         if(a==nullptr){return;}
-        else if(a->right==nullptr&&a->left==nullptr){sol.push_back(a->val);return;}
-        else if(a->left==nullptr){helper(sol,a->right);sol.push_back(a->val);return;}
-        else if(a->right==nullptr){helper(sol,a->left);sol.push_back(a->val);return;}
-        else{helper(sol,a->left);helper(sol,a->right);sol.push_back(a->val);return;}
+        else{
+            helper(sol,a->left);
+            helper(sol,a->right);
+            sol.push_back(a->val);
+            return;
+        }
     }
-    vector<int> postorderTraversal(TreeNode* root)  {
+    vector<int> postorderTraversal(TreeNode* root) {
         vector<int> sol;
         helper(sol,root);
         return sol;
     }
 };
-
