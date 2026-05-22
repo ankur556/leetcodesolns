21ae62f49f23dba3a4e0ac362cf4b39ee8170b8b
ankur556 <ankurclasher2@gmail.com>
2026-05-22 00:21:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.1 MB (96.32%)

--- solutions/0033-search-in-rotated-sorted-array/solution.cpp
+++ solutions/0033-search-in-rotated-sorted-array/solution.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int index=-1;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==target){index=i;}
+            if(nums[i]==target){
+                return i;
+            }
         }
-        if(index==-1){return -1;}
-        else{return index;}
+        return -1;
     }
 };
