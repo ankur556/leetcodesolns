57c9812c22cd6e3a8dd77342d4a18255b0eb5650
ankur556 <ankurclasher2@gmail.com>
2026-05-22 19:13:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 87.3 MB (45.22%)

--- solutions/0890-lemonade-change/solution.cpp
+++ solutions/0890-lemonade-change/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int no_of_5s=0;
-        int no_of_10s=0;
-        for(int i=0;i<bills.size();i++){
-            if(bills[i]==5){no_of_5s++;}
-            else if(bills[i]==10){no_of_10s++;no_of_5s--;}
+        int five=0;int tens=0;
+        for(int i:bills){
+            if(i==5){five++;}
+            else if(i==10){tens++;if(five==0){return false;}else{five--;}}
             else{
-                if(no_of_10s==0){no_of_5s-=3;}
-                else{no_of_5s--;no_of_10s--;}
+                if(tens>0&&five>0){
+                    tens--;five--;
+                }
+                else if(five>=3){five-=3;}
+                else{return false;}
             }
-            if(no_of_5s<0||no_of_10s<0){return false;}
         }
         return true;
     }
