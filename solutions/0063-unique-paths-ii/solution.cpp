05b468625be071d0d62f249d6d1f63152db3f960
ankur556 <ankurclasher2@gmail.com>
2026-05-24 21:40:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12 MB (6.19%)

---
+++ solutions/0063-unique-paths-ii/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        vector<vector<int>> dp=obstacleGrid;
+        dp[0][0]=1;
+        if(obstacleGrid[0][0]==1){return 0;}
+        for(int i=1;i<dp.size();i++){
+            dp[i][0]=dp[i-1][0];
+            if(obstacleGrid[i][0]==1){dp[i][0]=0;}
+        }
+        for(int i=1;i<dp[0].size();i++){
+            dp[0][i]=dp[0][i-1];
+            if(obstacleGrid[0][i]==1){dp[0][i]=0;}
+        }
+        for(int i=1;i<dp.size();i++){
+            for(int j=1;j<dp[0].size();j++){
+                if(obstacleGrid[i][j]==1){dp[i][j]=0;}
+                else{dp[i][j]=dp[i-1][j]+dp[i][j-1];}
+            }
+        }
+        return dp[dp.size()-1][dp[0].size()-1];
+    }
+};
