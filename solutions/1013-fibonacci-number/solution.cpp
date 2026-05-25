f4a03a63cba1aef5335fd5b311b1d5ecd263384b
ankur556 <ankurclasher2@gmail.com>
2026-05-25 20:09:53 +0000

    Sync LeetCode submission Runtime - 14 ms (22.87%), Memory - 7.7 MB (64.51%)

--- solutions/1013-fibonacci-number/solution.cpp
+++ solutions/1013-fibonacci-number/solution.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
-int fiB(int n){
-    if(n==1){return 1;}
-    else if(n==0){return 0;}
-    else{return fiB(n-1)+fiB(n-2);}
-}
     int fib(int n) {
-        return fiB(n);
+        if(n==0){return 0;}
+        if(n==1){return 1;}
+        return fib(n-1)+fib(n-2);
     }
 };
