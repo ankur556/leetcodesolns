3076b3c39df67354542c33f11f4baf942e634bfd
ankur556 <ankurclasher2@gmail.com>
2026-05-27 20:43:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 100.5 MB (33.37%)

---
+++ solutions/0932-monotonic-array/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& nums) {
+        bool inc=true;bool dec=true;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i+1]>nums[i]){dec=false;}
+            if(nums[i+1]<nums[i]){inc=false;}
+        }
+        if(inc||dec){return true;}
+        return false;
+    }
+};
