cd2d0a98bf0e4336cd6f9829aba2c789c23c6435
ankur556 <ankurclasher2@gmail.com>
2026-05-29 20:32:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.8 MB (22.68%)

--- solutions/0198-house-robber/solution.cpp
+++ solutions/0198-house-robber/solution.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()<=2){int max=0;
-        for(int i:nums){if(i>max){max=i;}}
-        return max;}
-        vector<int> helper(nums.size(),0);
-        helper[0]=nums[0];
-        helper[1]=max(nums[1],nums[0]);
-        for(int i=2;i<nums.size();i++){
-            helper[i]=max(helper[i-2]+nums[i],helper[i-1]);
+        if(nums.size()<=2){
+            if(nums.size()==0){return 0;}
+            else if(nums.size()==1){return nums[0];}
+            else{return max(nums[0],nums[1]);}
         }
-        return (int)helper[helper.size()-1];
+        vector<int> dp(nums.size(),0);
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[nums.size()-1];
     }
 };
