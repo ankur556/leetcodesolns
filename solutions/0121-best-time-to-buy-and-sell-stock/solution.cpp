922ce8bc8fabf230e2f1da5653b0280094ef1053
ankur556 <ankurclasher2@gmail.com>
2026-05-29 21:42:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 97.4 MB (60.49%)

--- solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-       int min_till=1e7;
-       int val=0;
-       for(int i: prices){
-       if(i<min_till){min_till=i;}
-       if(val<i-min_till){val=i-min_till;}
-       } 
-        return val;
+        int min_till=prices[0];
+        int max_pro=0;
+        for(int i=0;i<prices.size();i++){
+            if(prices[i]<min_till){min_till=prices[i];}
+            if(prices[i]-min_till>max_pro){max_pro=prices[i]-min_till;}
+        }
+        return max_pro;
     }
-   
 };
