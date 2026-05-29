f6acc27c27d7a9695bf4271d1ec9d48d87f4c690
ankur556 <ankurclasher2@gmail.com>
2026-05-29 21:38:22 +0000

    Sync LeetCode submission Runtime - 7 ms (5.60%), Memory - 17.1 MB (29.44%)

--- solutions/1036-rotting-oranges/solution.cpp
+++ solutions/1036-rotting-oranges/solution.cpp
@@ -2,32 +2,39 @@ class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<int,int>> q;
-        int nr=0;
-        int cnt=0;
+        vector<pair<int,int>>dirs;
+        int left=0;
+        vector<vector<int>> visited=grid;
+        dirs.push_back({0,1});
+        dirs.push_back({1,0});
+        dirs.push_back({-1,0});
+        dirs.push_back({0,-1});
+        int max=2;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
+                visited[i][j]=0;
                 if(grid[i][j]==2){q.push({i,j});}
-                if(grid[i][j]==1){nr++;}
+                else if(grid[i][j]==1){left++;}
             }
         }
-        int sol=0;
-        vector<pair<int, int>> dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        while(!q.empty()&&nr>0){
-            pair<int,int> a=q.front();
+       // int max=0;
+        while(!q.empty()){
+            int f=q.front().first;
+            int s=q.front().second;
             q.pop();
+            if(visited[f][s]==0){
+            visited[f][s]=1;
             for(auto dir:dirs){
-                int r=a.first;
-                int c=a.second;
-                int ra=a.first+dir.first;
-                int rc=a.second+dir.second;
-                if(ra>=0&&ra<grid.size()&&rc>=0&&rc<grid[0].size()){
-                    if(grid[ra][rc]==1){grid[ra][rc]=grid[r][c]+1;nr--;q.push({ra,rc});if(grid[ra][rc]>sol){sol=grid[ra][rc];}}
+                int fc=f+dir.first;
+                int sc=s+dir.second;
+                if(fc>=0&&fc<grid.size()&&sc>=0&&sc<grid[0].size()){
+                    if(grid[fc][sc]==1){grid[fc][sc]=grid[f][s]+1;q.push({fc,sc});left--;}
+                    if(grid[fc][sc]>max){max=grid[fc][sc];}
                 }
             }
         }
-        if(nr>0){return -1;}
-        else{
-            if(sol<2){return 0;}
-            return sol-2;}
+        }
+        if(left>0){return -1;}
+        else{return max-2;}
     }
 };
