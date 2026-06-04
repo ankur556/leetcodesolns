6d4bb0f1bf6950cabc95a7e9b7a4933672a6daec
ankur556 <ankurclasher2@gmail.com>
2026-06-04 21:19:31 +0000

    Sync LeetCode submission Runtime - 7 ms (0.28%), Memory - 23.3 MB (9.63%)

--- solutions/3476-find-minimum-operations-to-make-all-elements-divisible-by-three/solution.cpp
+++ solutions/3476-find-minimum-operations-to-make-all-elements-divisible-by-three/solution.cpp
@@ -1,11 +1,8 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-      int ans=0;
-    for(int i=0;i<nums.size();i++){
-        if(nums[i]%3==0){continue;}
-        else{ans++;}
-    }  
-    return ans;
+      int sol=0;
+        for(int i:nums){if(i%3==1){sol++;}else if(i%3==2){sol++;}}
+        return sol;
     }
 };
