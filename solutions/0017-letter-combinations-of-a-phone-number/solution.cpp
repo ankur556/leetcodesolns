a82b3529931f70ccaf0c172a056b34ba84bb347f
ankur556 <ankurclasher2@gmail.com>
2026-06-10 10:07:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.2 MB (77.43%)

--- solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
+++ solutions/0017-letter-combinations-of-a-phone-number/solution.cpp
@@ -1,37 +1,61 @@
 class Solution {
 public:
-    vector<string> letterCombinations(string digits) {
-        vector<string> res;
-        
-        if (digits.empty()) {
-            return res;
-        }
-        
-        unordered_map<char, string> digitToLetters = {
-            {'2', "abc"},
-            {'3', "def"},
-            {'4', "ghi"},
-            {'5', "jkl"},
-            {'6', "mno"},
-            {'7', "pqrs"},
-            {'8', "tuv"},
-            {'9', "wxyz"}
-        };
-        
-        backtrack(digits, 0, "", res, digitToLetters);
-        
-        return res;        
-    }
-
-    void backtrack(const string& digits, int idx, string comb, vector<string>& res, const unordered_map<char, string>& digitToLetters) {
-        if (idx == digits.length()) {
-            res.push_back(comb);
+    void helper(vector<vector<char>>&a,int & cur,int  cur_tot,string & digits,vector<string> & sol,string&curv){
+        if(cur_tot==digits.size()){if(curv.size()==digits.size()){sol.push_back(curv);}return;}
+        else{
+            for(int i=0;i<a[digits[cur_tot]-'2'].size();i++){
+                curv.push_back(a[digits[cur_tot]-'2'][i]);
+                helper(a,cur,cur_tot+1,digits,sol,curv);
+                curv.pop_back();
+            }
             return;
         }
-        
-        string letters = digitToLetters.at(digits[idx]);
-        for (char letter : letters) {
-            backtrack(digits, idx + 1, comb + letter, res, digitToLetters);
+    }
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) {
+            return {};
         }
-    }    
+        vector<string> sol;
+        int cur_tot=0;
+        int cur=0;
+        vector<vector<char>>a;
+        vector<char>b;
+        a.push_back(b);
+        a[a.size()-1].push_back('a');
+        a[a.size()-1].push_back('b');
+        a[a.size()-1].push_back('c');
+        a.push_back(b);
+        a[a.size()-1].push_back('d');
+        a[a.size()-1].push_back('e');
+        a[a.size()-1].push_back('f');
+        a.push_back(b);
+        a[a.size()-1].push_back('g');
+        a[a.size()-1].push_back('h');
+        a[a.size()-1].push_back('i');
+        a.push_back(b);
+        a[a.size()-1].push_back('j');
+        a[a.size()-1].push_back('k');
+        a[a.size()-1].push_back('l');
+        a.push_back(b);
+        a[a.size()-1].push_back('m');
+        a[a.size()-1].push_back('n');
+        a[a.size()-1].push_back('o');
+        a.push_back(b);
+        a[a.size()-1].push_back('p');
+        a[a.size()-1].push_back('q');
+        a[a.size()-1].push_back('r');
+        a[a.size()-1].push_back('s');
+        a.push_back(b);
+        a[a.size()-1].push_back('t');
+        a[a.size()-1].push_back('u');
+        a[a.size()-1].push_back('v');
+        a.push_back(b);
+        a[a.size()-1].push_back('w');
+        a[a.size()-1].push_back('x');
+        a[a.size()-1].push_back('y');
+        a[a.size()-1].push_back('z');
+        string t;
+        helper(a,cur,cur_tot,digits,sol,t);
+        return sol;
+    }
 };
