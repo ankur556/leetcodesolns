b0f81dfa6126cd6fabd8dde3de3c62fe5b97267f
ankur556 <ankurclasher2@gmail.com>
2026-06-13 07:42:45 +0000

    Sync LeetCode submission Runtime - 3 ms (75.18%), Memory - 19.1 MB (81.34%)

--- solutions/0207-course-schedule/solution.cpp
+++ solutions/0207-course-schedule/solution.cpp
@@ -1,36 +1,47 @@
 class Solution {
-private:
-    bool dfs(int node, const vector<vector<int>>& adj, vector<bool>& vis, vector<bool>& path) {
-        vis[node] = path[node] = true;
-
-        for (int next : adj[node]) {
-            if (!vis[next]) {
-                if (dfs(next, adj, vis, path)) return true;
-            } else if (path[next]) {
-                return true;
-            }
-        }
-        
-        path[node] = false;
-        return false;
-    }
-
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        // adj[i] holds all courses that depend on course 'i'
         vector<vector<int>> adj(numCourses);
-        for (const auto& pre : prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
+        // inDegree[i] holds the number of prerequisites course 'i' has
+        vector<int> inDegree(numCourses, 0);
+
+        // 1. Build the graph and in-degree array
+        for (auto& p : prerequisites) {
+            int course = p[0];
+            int prereq = p[1];
+            adj[prereq].push_back(course); 
+            inDegree[course]++; 
         }
 
-        vector<bool> vis(numCourses, false);
-        vector<bool> path(numCourses, false);
-
-        for (int i = 0; i < numCourses; ++i) {
-            if (!vis[i]) {
-                if (dfs(i, adj, vis, path)) return false;
+        // 2. Initialize the BFS queue with courses that have 0 prerequisites
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                q.push(i);
             }
         }
 
-        return true;
+        int coursesTaken = 0;
+
+        // 3. Process the queue (Standard BFS)
+        while (!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            coursesTaken++; // We successfully took this course
+
+            // For every course that depends on 'curr', reduce its requirement count
+            for (int neighbor : adj[curr]) {
+                inDegree[neighbor]--;
+                
+                // If it now has 0 prerequisites left, we can take it!
+                if (inDegree[neighbor] == 0) {
+                    q.push(neighbor);
+                }
+            }
+        }
+
+        // 4. If we were able to take all courses, there are no cycles
+        return coursesTaken == numCourses;
     }
 };
