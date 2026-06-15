f629a48eba6f2b77ea31efbd27f9bb9f35e0bb25
ankur556 <ankurclasher2@gmail.com>
2026-06-15 05:19:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 312 MB (83.23%)

--- solutions/2216-delete-the-middle-node-of-a-linked-list/solution.cpp
+++ solutions/2216-delete-the-middle-node-of-a-linked-list/solution.cpp
@@ -11,31 +11,15 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        
         ListNode* temp=head;
-        int size=1;
-        while(temp->next!=nullptr){size++;temp=temp->next;}
-
-        if(size>3){
+        int n=0;
+        while(temp!=nullptr){n++;temp=temp->next;}
+        if(n<=1){return nullptr;}
+        if(n==2){head->next=nullptr;return head;}
+        n=(n-2)/2;
         temp=head;
-        int muv=size/2;
-        for(int i=0;i<muv-1;i++){temp=temp->next;}
+        for(int i=0;i<n;i++){temp=temp->next;}
         temp->next=temp->next->next;
         return head;
-        }
-        else if(size==3){
-            temp=head;
-            temp->next=temp->next->next;
-            return head;
-        }
-        else if(size==2){
-            temp=head;
-            temp->next=nullptr;
-            return head;
-        }
-        else{
-            temp=nullptr;
-            return temp;
-        }
     }
 };
