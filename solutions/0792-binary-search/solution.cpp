381966c66c1b3e0cb8c357a2625909148708b428
ankur556 <ankurclasher2@gmail.com>
2026-07-05 21:24:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.3 MB (46.14%)

--- solutions/0792-binary-search/solution.cpp
+++ solutions/0792-binary-search/solution.cpp
@@ -1,42 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if(nums.size()==1){
-            if(nums[0]==target){return 0;}
-            else return -1;
-        }
-        else if(nums.size()==2){
-            if(nums[0]==target){return 0;
-            }
-            else if(nums[1]==target){
-                return 1;
-            }
-            else{return -1;}
-        }
-        else if(nums.size()==3){
-            if(nums[0]==target){return 0;
-            }
-            else if(nums[1]==target){
-                return 1;
-            }
-            else if(nums[2]==target){
-                return 2;
-            }
-            else{return -1;}
-        }
-        int smoll=0;
-        int high=nums.size();
-        int mid=0;
-        for(int i=1;i<nums.size()*2;i++){
-            mid=smoll+(high-smoll)/2;
-            if(target>nums[mid]){
-                smoll=mid;
-            }
-            else if(target<nums[mid]){
-                high=mid;
-            }
+        int l=0;int r=nums.size();
+        int mid=l+(r-l)/2;
+        while(l<r){
+            mid=l+(r-l)/2;
+            if(nums[mid]<target){l=mid+1;}
+            else if(nums[mid]>target){r=mid;}
             else{return mid;}
         }
+        // mid=l+(r-l)/2;
+        //if(nums[mid]==target){return mid;}
         return -1;
     }
 };
