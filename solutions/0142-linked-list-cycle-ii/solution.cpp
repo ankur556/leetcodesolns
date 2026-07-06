ef3cd0873051521cceade7ea09c1e911cfd7f26d
ankur556 <ankurclasher2@gmail.com>
2026-07-06 22:38:08 +0000

    Sync LeetCode submission Runtime - 11 ms (16.16%), Memory - 13.8 MB (7.27%)

--- solutions/0142-linked-list-cycle-ii/solution.cpp
+++ solutions/0142-linked-list-cycle-ii/solution.cpp
@@ -9,19 +9,9 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head!=nullptr&&head->next!=nullptr&&head->next->next!=nullptr){
-        ListNode* slow=head->next;
-        ListNode* fast=head->next->next;
-        
-        int n=0;
-        while(fast->next!=nullptr&&fast->next->next!=nullptr&&fast!=slow){slow=slow->next;fast=fast->next->next;}
-        if(fast->next==nullptr||fast->next->next==nullptr){return nullptr;}
-        else{
-        slow=head;
-        while(slow!=fast){slow=slow->next;fast=fast->next;}
-        return fast;
-        }
-        
-    }else{return nullptr;}
+        unordered_map<ListNode*,int> map;
+        while(head!=nullptr&&map[head]!=1){map[head]++;head=head->next;}
+        if(head==nullptr){return nullptr;}
+        else{return head;}
     }
 };
