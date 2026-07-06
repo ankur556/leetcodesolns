53210e5d11cdc8e17b3be9c50afb6104c100c6f2
ankur556 <ankurclasher2@gmail.com>
2026-07-06 22:32:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.5 MB (41.50%)

--- solutions/0206-reverse-linked-list/solution.cpp
+++ solutions/0206-reverse-linked-list/solution.cpp
@@ -10,15 +10,41 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* temp=head;
-        vector<int> sol;
-        while(temp!=nullptr){sol.push_back(temp->val);temp=temp->next;}
-        temp=head;
-        for(int i=sol.size()-1;i>=0;i--){
-            temp->val=sol[i];
+    ListNode* recursive(ListNode* node){
+        if(node==nullptr){return node;}
+        vector<ListNode*> a;
+        ListNode* temp=node;
+        while(temp!=nullptr){
+            a.push_back(temp);
             temp=temp->next;
         }
-        return head;
+        temp=a[a.size()-1];
+        node=temp;
+        for(int i=a.size()-2;i>=0;i--){
+            temp->next=a[i];temp=temp->next;
+        }
+        temp->next=nullptr;
+        return node;
+    }
+    ListNode* iti(ListNode*node){
+        ListNode* temp=node;
+        int n=0;
+        while(temp!=nullptr){n++;temp=temp->next;}
+        ListNode * og=node;
+        for(int i=n-1;i>=(n)/2;i--){
+            temp=node;
+            for(int j=0;j<i;j++){
+                temp=temp->next;
+            }
+            swap(og->val,temp->val);
+            og=og->next;
+        }
+        return node;
+    }
+    ListNode* reverseList(ListNode* head) {
+        //the recusive methord works
+        return recursive(head);
+       //iti methord
+       return iti(head);
     }
 };
