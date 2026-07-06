00684f37b0e306f65a5479f28f0a70b26f6d281d
ankur556 <ankurclasher2@gmail.com>
2026-07-06 12:18:32 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (64.02%)

--- solutions/0908-middle-of-the-linked-list/solution.cpp
+++ solutions/0908-middle-of-the-linked-list/solution.cpp
@@ -1,21 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int cnt = 0;
-
-        // Count the number of nodes
-        while (temp != nullptr) {
-            cnt++;
-            temp = temp->next;
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=nullptr&&fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
         }
-
-        int mid = cnt / 2;
-        temp = head;
-        for (int i = 0; i < mid; i++) {
-            temp = temp->next;
-        }
-        return temp;
+        return slow;
     }
 };
-
