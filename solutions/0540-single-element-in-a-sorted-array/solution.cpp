f2048bc9da437e1f84f1559c86f7ffe5906b4710
ankur556 <ankurclasher2@gmail.com>
2026-07-06 07:27:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.3 MB (34.66%)

--- solutions/0540-single-element-in-a-sorted-array/solution.cpp
+++ solutions/0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int val=-1;
-        if(nums.size()==1){return nums[0];}
-        for(int i=1;i<nums.size()-1;i++){
-            if(nums[i]==nums[i+1]||nums[i]==nums[i-1]){continue;}
-            else{val=nums[i];}
-        }
-        if(val!=-1){return val;}
-        else{
-            if(nums[0]!=nums[1]){return nums[0];}
-            else {return nums[nums.size()-1]; }
-        }
+        int sol=0;
+        for(int i: nums){sol^=i;}
+        return sol;
     }
 };
