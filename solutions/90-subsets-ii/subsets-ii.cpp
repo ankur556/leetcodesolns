c436fbadf5714464fe03dff03ffd9e65d4b23d03
ankur556 <ankurclasher2@gmail.com>
2026-07-08 16:02:17 +0530

    Time: 3 ms (32.46%) | Memory: 11.3 MB (18.38%) - LeetSync

--- solutions/90-subsets-ii/subsets-ii.cpp
+++ solutions/90-subsets-ii/subsets-ii.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
-    void backtrack(vector<vector<int>>& sol, vector<int>& current, vector<int>& nums, int start) {
-        sol.push_back(current);
-        
-        for (int i = start; i < nums.size(); ++i) {
-            // Skip duplicates: if the current number is the same as the previous one, skip it
-            if (i > start && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            
-            current.push_back(nums[i]);
-            backtrack(sol, current, nums, i + 1);
-            current.pop_back(); // Backtrack
+    void helper(vector<vector<int>> & sol,int cur,vector<int> & nums,int n,int lp,vector<int> & cv){
+        if(cur>=n){sol.push_back(cv);return;}
+        //if(lp==nums[cur]){}
+        else{
+            cv.push_back(nums[cur]);
+            helper(sol,cur+1,nums,n,lp,cv);
+            cv.pop_back();
+            helper(sol,cur+1,nums,n,lp,cv);
+            return;
         }
     }
-
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         vector<vector<int>> sol;
-        vector<int> current;
-        
-        // 1. Sort the input array to handle duplicates easily
-        sort(nums.begin(), nums.end()); 
-        
-        // 2. Start backtracking
-        backtrack(sol, current, nums, 0);
-        
-        return sol;
+        int cur=0;
+        sort(nums.begin(),nums.end());
+        int lastpushed=10e3;
+        vector<int> cv;
+        helper(sol,cur,nums,nums.size(),lastpushed,cv);
+        map<vector<int>,int> map;
+        vector<vector<int>> acc;
+        for(int i=0;i<sol.size();i++){
+            if(map[sol[i]]==0){acc.push_back(sol[i]);map[sol[i]]=1;}
+        }
+        return acc;
     }
 };
