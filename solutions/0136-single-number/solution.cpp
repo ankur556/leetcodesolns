b6f9f6365a294791f71a5921b8a16a14fef1a284
ankur556 <ankurclasher2@gmail.com>
2026-07-08 21:07:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.7 MB (58.74%)

--- solutions/0136-single-number/solution.cpp
+++ solutions/0136-single-number/solution.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int sol=0;
-        for(int i: nums){sol^=i;}
-        return sol;
+       int sol=nums[0];
+       for(int i=1;i<nums.size();i++) {sol^=nums[i];}
+       return sol;
     }
 };
