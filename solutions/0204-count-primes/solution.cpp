f67b7dfdf5686ff9f3feb7c53ed60ae683c318bd
ankur556 <ankurclasher2@gmail.com>
2026-07-08 21:57:21 +0000

    Sync LeetCode submission Runtime - 91 ms (98.08%), Memory - 12.7 MB (64.77%)

--- solutions/0204-count-primes/solution.cpp
+++ solutions/0204-count-primes/solution.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        int cnt = 0;
-        vector<bool> prime(n + 1, true);
-        prime[0] = prime[1] = false;
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) {
-                cnt++;
-                for (int j = i * 2; j < n; j = j + i) {
-                    prime[j] = 0;
+        if (n <= 2) return 0;
+        vector<bool> isPrime(n, true);
+        int ans = 1; 
+        
+        for (int i = 3; i * i < n; i += 2) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += 2 * i) {
+                    isPrime[j] = false;
                 }
             }
         }
-        return cnt;
+        
+        for (int i = 3; i < n; i += 2) {
+            if (isPrime[i]) {
+                ans++;
+            }
+        }
+        
+        return ans;
     }
 };
