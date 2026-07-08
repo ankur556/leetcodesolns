8e0a42184c00d1bb475013b910f6b0d71563e5d6
ankur556 <ankurclasher2@gmail.com>
2026-07-08 23:52:38 +0000

    Sync LeetCode submission Runtime - 7 ms (35.20%), Memory - 28.4 MB (83.66%)

--- solutions/0503-next-greater-element-ii/solution.cpp
+++ solutions/0503-next-greater-element-ii/solution.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int> a;
-        for(int i=0;i<nums.size();i++){a.push_back(nums[i]);}
-        for(int i=0;i<nums.size();i++){a.push_back(nums[i]);}
-        stack<int> b;
-        vector<int> sol(2*nums.size());
-        for(int i=a.size()-1;i>=0;i--){
-            while(!b.empty()&&a[b.top()]<=a[i]){b.pop();}
-            if(b.empty()){sol[i]=-1;b.push(i);}
-            else{sol[i]=a[b.top()];b.push(i);}
-        }
-        for(int i=0;i<nums.size();i++){
-            nums[i]=sol[i];
-        }
-        return nums;
+       int n=nums.size();
+       int max=0;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]>nums[max]){max=i;}
+       }
+       cout<<max<<endl;
+       stack<int> s;
+       s.push(nums[max]);
+       nums[max]=-1;
+       for(int i=max-1;i>=0;i--){
+        while(!s.empty()&&s.top()<=nums[i]){s.pop();}
+        if(s.empty()){s.push(nums[i]);nums[i]=-1;cout<<"np"<<endl;}
+        else{int k=nums[i];
+        nums[i]=s.top();
+        s.push(k);}
+       }
+       for(int i=nums.size()-1;i>=max+1;i--){
+         while(!s.empty()&&s.top()<=nums[i]){s.pop();}
+        if(s.empty()){s.push(nums[i]);nums[i]=-1;}
+        else{int k=nums[i];
+        nums[i]=s.top();
+        s.push(k);}
+       }
+       return nums;
     }
 };
