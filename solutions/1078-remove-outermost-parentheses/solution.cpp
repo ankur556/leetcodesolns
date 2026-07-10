56167b0b0c64da9f43d88a41f296f0fd34367d13
ankur556 <ankurclasher2@gmail.com>
2026-07-10 22:21:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (54.81%)

--- solutions/1078-remove-outermost-parentheses/solution.cpp
+++ solutions/1078-remove-outermost-parentheses/solution.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        int cnt=0;
-        string sol;
-        for(char c:s){if(cnt!=0&&c=='('){sol.push_back(c);}if(c=='('){cnt++;}else{cnt--;}if(cnt!=0&&c==')'){sol.push_back(c);}}
-        return sol;
+       int c=1;
+       string sol;
+       bool flag=false;
+       for(int i=1;i<s.size();i++){
+        if(s[i]=='('){c++;}
+        else{c--;}
+        if(c==0||(c==1&&s[i]=='(')){}
+        else{sol.push_back(s[i]);}
+       } 
+       return sol;
     }
 };
