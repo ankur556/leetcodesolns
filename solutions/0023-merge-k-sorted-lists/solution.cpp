c73641372d5560b43b44a80959abd87d936806d4
ankur556 <ankurclasher2@gmail.com>
2026-07-10 07:02:42 +0000

    Sync LeetCode submission Runtime - 44 ms (15.46%), Memory - 19.1 MB (19.45%)

--- solutions/0023-merge-k-sorted-lists/solution.cpp
+++ solutions/0023-merge-k-sorted-lists/solution.cpp
@@ -1,37 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-     
-        vector<int> a;
-        for(ListNode* list : lists) { 
-            ListNode* curr = list;
-            while(curr != nullptr) {
-                a.push_back(curr->val);
-                curr = curr->next;
+        if(lists.size()==0){return nullptr;}
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int i=0;i<lists.size();i++){
+            ListNode* temp=lists[i];
+            while(temp!=nullptr){
+                //cout<<temp->val;
+                pq.push(temp->val);
+                temp=temp->next;
+                //cout<<pq.top()<<endl;
             }
         }
-
-        sort(a.begin(), a.end());
-
-        if (a.empty()) {
-            return nullptr;
+        //vector<ListNode*> sol;
+        //if(sol.size()==0){return nullptr;}
+        if(pq.empty()){return nullptr;}
+        ListNode* n=new   ListNode(pq.top());
+        pq.pop();
+        ListNode* ne=n;
+        while(!pq.empty()){            
+           // cout<<"no issue wt loop?"<<endl;
+            ListNode* nq=new  ListNode(pq.top());
+            pq.pop();
+            ne->next=nq;
+            ne=nq;
+            cout<<pq.top()<<endl;
         }
-
-        ListNode* dummyHead = new ListNode(0);
-
-        ListNode* helper = dummyHead; 
-
-        for(int val : a) {
-
-            helper->next = new ListNode(val);
-
-            helper = helper->next;
-        }
- 
-        ListNode* ans = dummyHead->next;
-        
-        delete dummyHead;
-        
-        return ans; 
+        return n;
     }
 };
