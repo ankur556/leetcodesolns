8015a2370ad0bf4a48e8a0f14f3352a9774de615
ankur556 <ankurclasher2@gmail.com>
2026-07-12 20:43:31 +0000

    Sync LeetCode submission Runtime - 12 ms (96.22%), Memory - 17.8 MB (84.27%)

--- solutions/0322-coin-change/solution.cpp
+++ solutions/0322-coin-change/solution.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount+1,-1);
-        if(amount==0){return 0;}
+        vector<int> dp(amount+1,1e7);
         dp[0]=0;
-        bool p=false;
-        for(int i: coins){if(i<amount){p=true;break;}if(i==amount){return 1;}}
-        if(!p){return -1;}
         for(int i=0;i<coins.size();i++){
-            if(coins[i]<amount){
-            dp[coins[i]]=1;}
-        }
-        for(long long i=0;i<coins.size();i++){
-            for(long long j=0;j<=amount;j++){
-                if(j+coins[i]<=amount){
-                if(dp[j+coins[i]]==-1&&dp[j]!=-1){dp[j+coins[i]]=dp[j]+1;}
-                if(dp[j+coins[i]]>dp[j]+1&&dp[j]!=-1){dp[j+coins[i]]=dp[j]+1;}}
+            for(int j=coins[i];j<dp.size();j++){
+                dp[j]=min(dp[j-coins[i]]+1,dp[j]);
+                //cout<<dp[j]<<" "<<j<<endl;
             }
         }
-        return dp[amount];
+        if(dp[dp.size()-1]==1e7){return -1;}
+        else{return dp[dp.size()-1];}
     }
 };
