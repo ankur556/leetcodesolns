28862df3f51c4c37ffeb625fe179858c162951ea
ankur556 <ankurclasher2@gmail.com>
2026-07-12 11:49:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (70.70%)

--- solutions/0062-unique-paths/solution.cpp
+++ solutions/0062-unique-paths/solution.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        // Calculate C(m+n-2, n-1) or C(m+n-2, m-1)
-        int N = m + n - 2;
-        int k = min(m - 1, n - 1);  // choose smaller to reduce iterations
-        unsigned long long result = 1;
-
-        for (int i = 1; i <= k; ++i) {
-            result = result * (N - k + i) / i;
+        vector<vector<int>> dp(n,vector<int>(m));
+        dp[0][0]=1;
+        if(n==0||m==0){return 0;}
+        if(n==1||m==1){return 1;}
+        for(int i=0;i<n;i++){dp[i][0]=1;}
+        for(int i=0;i<m;i++){dp[0][i]=1;}
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
         }
-
-        return (int)result;
+    return dp[n-1][m-1];
     }
 };
-
