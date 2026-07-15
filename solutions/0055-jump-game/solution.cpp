bec9d6137ae92d9796814ba4c0bbb6ebcc0bd149
ankur556 <ankurclasher2@gmail.com>
2026-07-15 10:27:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 52.4 MB (58.63%)

--- solutions/0055-jump-game/solution.cpp
+++ solutions/0055-jump-game/solution.cpp
@@ -1,1 +1,11 @@
-class Solution {public:bool canJump(vector<int>& nums){int maxi=0;if(nums.size()==1){return true;}else if(nums.size()==2){if(nums[0]>=1){return true;}}for(int i=0;i<nums.size();i++){if(maxi<i){return false;}maxi=max(maxi,nums[i]+i);if(maxi>nums.size()-2){return true;}}return false;}};
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > maxReach) return false;
+            maxReach = max(maxReach, i + nums[i]);
+        }
+        return true;
+    }
+};
