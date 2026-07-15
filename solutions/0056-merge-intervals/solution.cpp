8536a3a349b20c62fab161aa582597912e7443e7
ankur556 <ankurclasher2@gmail.com>
2026-07-15 08:05:49 +0000

    Sync LeetCode submission Runtime - 3 ms (88.90%), Memory - 23.9 MB (30.69%)

--- solutions/0056-merge-intervals/solution.cpp
+++ solutions/0056-merge-intervals/solution.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        if(intervals.size()==0){return intervals;}
         sort(intervals.begin(),intervals.end());
-        int max=intervals[0][1];
-        int min=intervals[0][0];
+        int start_interval=intervals[0][0];int end_interval=intervals[0][1];
         vector<vector<int>> sol;
         for(int i=0;i<intervals.size();i++){
-            if(intervals[i][0]<=std::max(max,min)){
-                max=std::max(intervals[i][1],max);
-                min=std::min(intervals[i][0],min);
+            if(intervals[i][0]<=end_interval){
+                start_interval=min(intervals[i][0],start_interval);
+                end_interval=max(intervals[i][1],end_interval);
             }
             else{
-                vector<int> a;
-                sol.push_back(a);
-                sol[sol.size()-1].push_back(min);
-                sol[sol.size()-1].push_back(max);
-                min=intervals[i][0];
-                max=intervals[i][1];
+                sol.push_back({start_interval,end_interval});
+                start_interval=intervals[i][0];end_interval=intervals[i][1];
+
             }
         }
-        vector<int> a;
-                sol.push_back(a);
-                sol[sol.size()-1].push_back(min);
-                sol[sol.size()-1].push_back(max);
-                return sol;
+        sol.push_back({start_interval,end_interval});
+        return sol;
     }
 };
