68f1a4bdbb6538376e16171e0742fa77ef508b30
ankur556 <ankurclasher2@gmail.com>
2026-07-22 21:12:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (89.92%) - LeetSync

--- solutions/198-house-robber/house-robber.cpp
+++ solutions/198-house-robber/house-robber.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if(nums.size()==0){return 0;}
-        if(nums.size()==1){return nums[0];}
-        int prev = 0;int cur = nums[0];int oldcur=0;
-        for(int i=1;i<nums.size();i++){
-            oldcur=cur;
-            cur=max(prev+nums[i],cur);
-            prev=max(prev,oldcur);
-        }
-        return cur;
+      int rob=0;
+      int nrob=0;
+      int lnrob=0;
+      for(int i=0; i < nums.size();i++){
+        lnrob=nrob;
+        nrob=max(rob,nrob);
+        rob=max(rob,lnrob+nums[i]);
+      }  
+      return rob;
     }
 };
