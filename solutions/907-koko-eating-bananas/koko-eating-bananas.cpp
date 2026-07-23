3b61fc4a4b30177c688ef6ddac2b1957cbc3b2c3
ankur556 <ankurclasher2@gmail.com>
2026-07-23 12:14:06 +0530

    Time: 8 ms (54.66%) | Memory: 39.6 MB (5.03%) - LeetSync

--- solutions/907-koko-eating-bananas/koko-eating-bananas.cpp
+++ solutions/907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    bool isval(long mid,vector<int>& piles,int h){
-        if(mid==0){return false;}
-        long long cnt=0;
-        for(int i: piles){if(i%mid>0){cnt+=(i/mid+1);}else{cnt+=(i/mid);}}
-        return cnt<=h;
+    bool isval(long long ver,vector<int> piles, int tar){
+        long long cur=0;
+        if(ver==0){return false;}
+        for(int i: piles){
+            cur+=i/ver;if(i%ver!=0){cur++;}
+        }
+        return cur<=tar;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int r=1e9;
-        int l=0;
-        int mid=l+(r-l)/2;
-        while(l<r){
-            mid=l+(r-l)/2;
-            if(isval(mid,piles,h)){r=mid;}
-            else{l=mid+1;}
-        }
-        return l+(r-l)/2;
+      long long r=1e9;long long l=0;
+      long mid=l+(r-l)/2;
+      while(l<r){
+        mid=l+(r-l)/2;
+        if(isval(mid,piles,h)){r=mid;}
+        else{l=mid+1;}
+      }  
+      return l;
     }
 };
