7af9d7aaf649622889eac7bd117391a6f6e017d1
ankur556 <ankurclasher2@gmail.com>
2026-07-26 11:33:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (81.32%) - LeetSync

--- solutions/2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
+++ solutions/2710-minimum-operations-to-reduce-an-integer-to-0/minimum-operations-to-reduce-an-integer-to-0.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
     int minOperations(int n) {
-        int ops = 0;
-        while (n > 0) {
-            // If the last two bits end in '11' (i.e., multiple consecutive ones)
-            if ((n & 3) == 3) {
-                n += 1;
-                ops++;
-            } 
-            // If the last bit is 1 and the second to last is 0 (i.e., a single isolated 1)
-            else if ((n & 3) == 1) {
-                n -= 1;
-                ops++;
-            } 
-            // If the last bit is 0, just shift right
-            else {
-                n >>= 1;
+        int sol=0;
+        while(n>0){
+            if(n%2){
+                if((n/2)%2){n++;}
+                else{n--;}
+                sol++;
             }
+            else{n/=2;}
         }
-        return ops;
+        return sol;
     }
 };
