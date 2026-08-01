6ff61ed35defa64a4bc4de2fb4cafa7e035d8699
ankur556 <ankurclasher2@gmail.com>
2026-08-02 00:16:31 +0530

    Time: 3 ms (25.99%) | Memory: 17.3 MB (17.06%) - LeetSync

--- solutions/1036-rotting-oranges/rotting-oranges.cpp
+++ solutions/1036-rotting-oranges/rotting-oranges.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int init=0;
+        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size()));
         queue<pair<int,pair<int,int>>> q;
-        int st=0;
+        int sol=0;
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
-                if(grid[i][j]==2){q.push({0,{i,j}});}
-                else if(grid[i][j]==1){st++;}
-            }
-        }
-        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size()));
-        vector<pair<int,int>> dirs={{1,0},{0,1},{-1,0},{0,-1}};
-        int sol=0;
-        //int cnt=0;
-        while(!q.empty()){
-            int val=q.front().first;
-            int i1=q.front().second.first;
-            int i2=q.front().second.second;
-            q.pop();
-            if(val>sol){sol=val;}
-            vis[i1][i2]=1;
-            for(auto a: dirs){
-                int a1=i1+a.first;
-                int a2=i2+a.second;
-                if(a1>=0&&a1<grid.size()&&a2>=0&&a2<grid[0].size()){
-                    if(vis[a1][a2]==0&&grid[a1][a2]==1){
-                        grid[a1][a2]=2;
-                        vis[a1][a2]=1;
-                        q.push({val+1,{a1,a2}});
-                        st--;
-                    }
+               if(grid[i][j]==2){ 
+                q.push({0,{i,j}});}
+                else if(grid[i][j]==1){
+                    init++;
                 }
             }
         }
-        if(st==0)return sol;
-        return -1;
+        vector<pair<int,int>> dirs={{0,1},{1,0},{0,-1},{-1,0}};
+        while(!q.empty()){
+            int i=q.front().second.first;
+            int j=q.front().second.second;
+            int cost=q.front().first;
+            q.pop();
+            sol=max(sol,cost);
+            for(auto a: dirs){
+                int x=i+a.first;
+                int y=j+a.second;
+                if(x>=0&&x<grid.size()&&y>=0&&y<grid[0].size()&&vis[x][y]==0){
+                    if(grid[x][y]==1){init--;grid[x][y]=2;q.push({cost+1,{x,y}});}
+                }
+            }
+        }
+        if(init==0){return sol;}
+        else{return -1;}
     }
 };
