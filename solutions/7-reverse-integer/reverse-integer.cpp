240dd0158bb8b879f386bc72b980847cab11aaae
ankur556 <ankurclasher2@gmail.com>
2026-08-01 17:31:20 +0530

    Time: 2 ms (38.17%) | Memory: 8.5 MB (54.83%) - LeetSync

--- solutions/7-reverse-integer/reverse-integer.cpp
+++ solutions/7-reverse-integer/reverse-integer.cpp
@@ -5,13 +5,11 @@ public:
         long long x=x1;
         if(x<0){neg=true;x*=-1;}
         vector<int> helper;
-        while(x>0){
-            helper.push_back(x%10);x/=10;
-        }
         long long sol=0;
-        for(int i=0;i<helper.size();i++){
-           sol*=10; sol+=helper[i];
+        while(x>0){
+            sol*=10; sol+=x%10;x/=10;
         }
+        
         if(!(pow(2,31)-1>sol&&-pow(2,31)<sol)){return 0;}
         if(neg){return -sol;}
         return sol;
