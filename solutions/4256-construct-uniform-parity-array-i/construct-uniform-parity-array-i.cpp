b677a5762d135e4f914a8d2f9f1d22c43154277a
ankur556 <ankurclasher2@gmail.com>
2026-09-02 09:50:35 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (94.95%) - LeetSync

---
+++ solutions/4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        // if we want odd then we need atleast 2 odds and no of evens>2
+        // if we want even odd>2 and even > 2 
+        // if there is one odd
+        // then t
+        // if there is one odd one even we can make 2 odds
+        return true;
+    }
+};
