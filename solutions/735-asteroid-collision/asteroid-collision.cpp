d03e6c9c788f935aa8a41f886950d40b92cd3946
ankur556 <ankurclasher2@gmail.com>
2026-09-04 12:38:45 +0530

    Time: 0 ms (100.00%) | Memory: 22.2 MB (41.04%) - LeetSync

--- solutions/735-asteroid-collision/asteroid-collision.cpp
+++ solutions/735-asteroid-collision/asteroid-collision.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& a) {
-        stack<int> s;
+    vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> sol;
-        bool val=false;
-        for(int i=0;i<a.size();i++){
-            if(a[i]>0){s.push(a[i]);}
+        stack<int> s;
+        for(int i : asteroids){
+            if(i>0){s.push(i);}
             else{
-                val=false;
-                while(!s.empty()&&s.top()<=-a[i]){///equal case not dealt with yet
-                    if(s.top()==(-a[i])){cout<<s.top()<<endl;s.pop();val=true;break;}
-                    else{
-                        s.pop();
-                    }
-                    }
-                    if(s.empty()&&!val){sol.push_back(a[i]);}
-                }
+                while(!s.empty()&&s.top()<-i){s.pop();}
+                if(s.empty()){sol.push_back(i);}
+                else if(s.top()==-i){s.pop();}
             }
-            stack<int> t;
-        while(!s.empty()){
-            t.push(s.top());s.pop();
         }
-        while(!t.empty()){
-            sol.push_back(t.top());t.pop();
-        }
+        stack<int> s2;
+        while(!s.empty()){s2.push(s.top());s.pop();}
+        while(!s2.empty()){sol.push_back(s2.top());s2.pop();}
         return sol;
     }
 };
