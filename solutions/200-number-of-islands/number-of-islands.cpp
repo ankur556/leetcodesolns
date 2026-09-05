d0b67e849b772853910140b10fb16498ec444d77
ankur556 <ankurclasher2@gmail.com>
2026-09-05 15:39:36 +0530

    Time: 27 ms (55.48%) | Memory: 25.6 MB (7.43%) - LeetSync

--- solutions/200-number-of-islands/number-of-islands.cpp
+++ solutions/200-number-of-islands/number-of-islands.cpp
@@ -1,44 +1,43 @@
 class Solution {
 public:
-    vector<vector<int>> dirs={{0,1},{0,-1},{-1,0},{1,0}};
-    void bfs(vector<vector<char>>& grid,vector<vector<int>>& vis,int x,int y){
-        queue<pair<int,int>> q;
-        q.push({x,y});
-        while(!q.empty()){
-            int i=q.front().first;
-            int j=q.front().second;
-            q.pop();
+    vector<pair<int,int>> dirs={{0,1},{0,-1},{1,0},{-1,0}};
+    void bfs(vector<vector<char>> & grid,vector<vector<int>> & vis ,int x ,int y){
+        queue<pair<int,int>> q1;
+        q1.push({x,y});
+        while(!q1.empty()){
+            int i=q1.front().first;
+            int j=q1.front().second;
+            q1.pop();
             if(vis[i][j]==0){
                 vis[i][j]=1;
                 for(auto a : dirs){
-                    int xd=i+a[0];
-                    int yd=j+a[1];
-                    if(xd<grid.size()&&xd>=0&&yd>=0&&yd<grid[0].size()&&grid[xd][yd]=='1'){
-                        if(vis[xd][yd]==0){
-                        q.push({xd,yd});}}
+                    int d1=i+a.first;
+                    int d2=j+a.second;
+                    if(d1>=0&&d1<grid.size()&&d2>=0&&d2<grid[0].size()&&grid[d1][d2]=='1'){
+                        if(vis[d1][d2]==0){q1.push({d1,d2});}
+                    }
                 }
             }
         }
     }
     int numIslands(vector<vector<char>>& grid) {
-      int sol=0;
-      int n=grid.size();int m=grid[0].size();
-      queue<pair<int,int>> q;
-      vector<vector<int>> vis(n,vector<int>(m));
-      for(int i=0;i<grid.size();i++){
-        for(int j=0;j<grid[0].size();j++){
-            if(grid[i][j]=='1'){q.push({i,j});}
+        queue<pair<int,int>> q;
+        vector<vector<int>> vis(grid.size(),vector<int>(grid[0].size()));
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]=='1'){q.push({i,j});}
+            }
         }
-      }  
-      while(!q.empty()){
-        int i=q.front().first;int j=q.front().second;
-        q.pop();
-        if(vis[i][j]==0){
-            sol++;
-            bfs(grid,vis,i,j);
-            vis[i][j]=1;
+        int sol=0;
+        while(!q.empty()){
+            int x=q.front().first;int y=q.front().second;
+            q.pop();
+            if(vis[x][y]==0){
+                sol++;
+                bfs(grid,vis,x,y);
+                vis[x][y]=1;
+            }
         }
-      }
-      return sol;
+        return sol;
     }
 };
